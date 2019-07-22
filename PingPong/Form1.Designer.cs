4d82495dfe99e7e03ec40b3a31473966a7d96ad4
AlexandrTUR <valeralox7@mail.ru>
2019-07-22 14:55:04 +0300

    Initial commit

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.Panel();
+            this.result = new System.Windows.Forms.Label();
+            this.gameBol = new System.Windows.Forms.PictureBox();
+            this.gamePanel = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.loselabel = new System.Windows.Forms.Label();
+            this.background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.background.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.background.Controls.Add(this.loselabel);
+            this.background.Controls.Add(this.result);
+            this.background.Controls.Add(this.gameBol);
+            this.background.Controls.Add(this.gamePanel);
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(800, 450);
+            this.background.TabIndex = 0;
+            // 
+            // result
+            // 
+            this.result.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(640, 9);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(132, 25);
+            this.result.TabIndex = 2;
+            this.result.Text = "Результат 0";
+            // 
+            // gameBol
+            // 
+            this.gameBol.BackColor = System.Drawing.Color.Red;
+            this.gameBol.Location = new System.Drawing.Point(269, 49);
+            this.gameBol.Name = "gameBol";
+            this.gameBol.Size = new System.Drawing.Size(50, 50);
+            this.gameBol.TabIndex = 1;
+            this.gameBol.TabStop = false;
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.SystemColors.ControlText;
+            this.gamePanel.Location = new System.Drawing.Point(253, 413);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(99, 25);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // loselabel
+            // 
+            this.loselabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loselabel.AutoSize = true;
+            this.loselabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loselabel.Location = new System.Drawing.Point(317, 200);
+            this.loselabel.Name = "loselabel";
+            this.loselabel.Size = new System.Drawing.Size(311, 47);
+            this.loselabel.TabIndex = 3;
+            this.loselabel.Text = "Вы проиграли!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.background.ResumeLayout(false);
+            this.background.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox gamePanel;
+        private System.Windows.Forms.PictureBox gameBol;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label loselabel;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        private int speed_ver = 2;
+        private int speed_hor = 2;
+        private int score = 0;
+                    
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Enabled = true; // Если значение true,то мы можем отслеживать различные передвиженеия 
+
+            Cursor.Hide(); // скрытие курсора во время игры
+            this.FormBorderStyle = FormBorderStyle.None;  //  убираем обводку окна
+            this.TopMost = true; // позволяет программе быть всегда поверх других программ
+
+            this.Bounds = Screen.PrimaryScreen.Bounds; // когда запуститься программа, то размеры прлограммы будут одинаковы по ширине и по высоте
+
+            gamePanel.Top = background.Bottom - (background.Bottom / 10); // gamepanel будет находиться снизу
+
+            loselabel.Visible = false;
+            loselabel.Left = (background.Width/2) - (loselabel.Width/2);
+            loselabel.Top = (background.Height / 2) - (loselabel.Height / 2);
+        }
+                     
+        private void Form1_KeyDown(object sender, KeyEventArgs e) //если нажали escape,то закрыть окно с программой
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+            if (e.KeyCode == Keys.Enter)
+            {
+                gameBol.Top = 50;
+                gameBol.Left = 70;
+                speed_hor = 2;
+                speed_ver = 2;
+                score = 0;
+                loselabel.Visible = false;
+                timer.Enabled = true;
+                result.Text = "Результат: 0";
+
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            gamePanel.Left = Cursor.Position.X - (gamePanel.Width / 2);
+
+            gameBol.Left += speed_hor;
+            gameBol.Top += speed_ver;
+
+            if (gameBol.Left <= background.Left)
+                speed_hor *= -1;
+
+            if (gameBol.Right >= background.Right)
+                speed_hor *= -1;
+
+            if (gameBol.Top <= background.Top)
+                speed_ver *= -1;
+
+            if (gameBol.Bottom >= background.Bottom)
+            {
+                loselabel.Visible = true;
+                timer.Enabled = false;
+            }
+
+            if (gameBol.Bottom >= gamePanel.Top && gameBol.Bottom <= gamePanel.Bottom
+                                              && gameBol.Left >= gamePanel.Left
+                                              && gameBol.Right <= gamePanel.Right)
+            {
+                speed_hor += 2;
+                speed_ver += 2;
+                speed_ver *= -1;
+                score += 1;
+                result.Text = "Резульатат: " + score.ToString();
+
+                Random randColor = new Random();
+                background.BackColor = Color.FromArgb(randColor.Next(150,255), randColor.Next(150, 255), randColor.Next(150, 255));
+            }
+
+        }
+
+        
+    }
+}
+
